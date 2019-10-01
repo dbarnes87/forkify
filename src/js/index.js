ccc6fe3932339d44c240300765c41d2fa772a000
David Barnes <david.barnes115@gmail.com>
2019-10-01 14:15:06 -0400

    built search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const key = 'b8c5eecaa2ad09375fcf9240997eee57'
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
-getResults('tomato pasta');
-
-// b8c5eecaa2ad09375fcf9240997eee57
-// https://www.food2fork.com/api/search
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
