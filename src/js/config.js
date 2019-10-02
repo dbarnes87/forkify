3e529dbf0de47cf93c12bebe3cb3953638d454ad
David Barnes <david.barnes115@gmail.com>
2019-10-02 14:04:53 -0400

    built recipe controller

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,4 @@
 
-export const key = 'b8c5eecaa2ad09375fcf9240997eee57';
+// export const key = 'b8c5eecaa2ad09375fcf9240997eee57';
+export const key = 'eab3793dc934ee3a02a09606e54b4528';
+
--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,8 @@ const state = {};
 */
 const controlSearch = async () => {
     // 1) Get query from view
-    const query = searchView.getInput();
+    const query = 'pizza';
+    // const query = searchView.getInput();
     console.log(query);
 
     if(query) {
@@ -29,12 +30,17 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4) Search for recipes
-        await state.search.getResults();
+        try {
+            // 4) Search for recipes
+            await state.search.getResults();
 
-        // 5) Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5) Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Error processing search...');
+            clearLoader();
+        }
     }
 }
 
@@ -42,6 +48,13 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
+
+// TESTING
+window.addEventListener('load', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
 
 elements.searchResPages.addEventListener('click', e => {
     const btn = e.target.closest('.btn-inline');
@@ -56,6 +69,37 @@ elements.searchResPages.addEventListener('click', e => {
 /*
 * RECIPE CONTROLLER
 */
-const r = new Recipe(46956);
-r.getRecipe();
-console.log(r);
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if (id) {
+        // Prepare the UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+        
+        // TESTING
+        window.r = state.recipe;
+
+        try {
+
+            // Get recipe data
+            await state.recipe.getRecipe();
+
+            // Calc time and servings
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            //  Render recipe
+            console.log(state.recipe);
+        } catch (err) {
+            alert('Error processing recipe!');
+        }
+    }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
