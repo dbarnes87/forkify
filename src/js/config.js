8f55db86984066f4a0970743cd0dce0c04e479f8
David Barnes <david.barnes115@gmail.com>
2019-10-03 13:31:41 -0400

    built likes view

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,7 @@
 
 // export const key = 'b8c5eecaa2ad09375fcf9240997eee57';
-export const key = 'eab3793dc934ee3a02a09606e54b4528';
+// export const key = 'eab3793dc934ee3a02a09606e54b4528';
 // export const key = '2be2b1a71f749c6df5e1d8ec7f8cf6c9';
+export const key = '12e71bac6d2523a5ba61262b0951d11b';
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 
@@ -95,9 +96,13 @@ const controlRecipe = async () => {
 
             //  Render recipe
             clearLoader();
-            recipeView.renderRecipe(state.recipe);
+            recipeView.renderRecipe(
+                state.recipe,
+                state.likes.isLiked(id)
+            );
 
         } catch (err) {
+            console.log(err);
             alert('Error processing recipe!');
         }
     }
@@ -141,6 +146,10 @@ elements.shopping.addEventListener('click', e => {
 /*
 LIKE Controller
 */
+// Testing
+state.likes = new Likes();
+likesView.toggleLikeMenu(state.likes.getNumLikes());
+
 const controlLike= () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -152,23 +161,24 @@ const controlLike= () => {
             currentID,
             state.recipe.title,
             state.recipe.author,
-            state.recipe.img
+            state.recipe.image
         );
         // Toggle the like button
-
+        likesView.toggleLikedBtn(true);
         // Add like to UI list
-            console.log(state.likes);
+        likesView.renderLike(newLike);    
     // User has liked current recipe
     } else {
         // Remove like to the state
         state.likes.deleteLike(currentID);
     
         // Toggle the like button
+        likesView.toggleLikedBtn(false);
 
         // Remove like to UI list
-        console.log(state.likes);
-
+        likesView.deleteLike(currentID);
     }
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
 
 
