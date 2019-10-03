6f91618524552719921ae4fad26081ccb85c661e
David Barnes <david.barnes115@gmail.com>
2019-10-03 11:40:54 -0400

    built list controller

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 
-export const key = 'b8c5eecaa2ad09375fcf9240997eee57';
-// export const key = 'eab3793dc934ee3a02a09606e54b4528';
+// export const key = 'b8c5eecaa2ad09375fcf9240997eee57';
+export const key = 'eab3793dc934ee3a02a09606e54b4528';
 // export const key = '2be2b1a71f749c6df5e1d8ec7f8cf6c9';
 
 
--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -2,10 +2,10 @@ import { elements } from './base';
 
 export const renderItem = item => {
     const markup = `
-        <li class="shopping__item" data-itemid="${item.id}>
+        <li class="shopping__item" data-itemid="${item.id}">
             <div class="shopping__count">
                 <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
-                <p>${item.unit}</p>
+            <p>${item.unit}</p>
             </div>
             <p class="shopping__description">${item.ingredient}</p>
             <button class="shopping__delete btn-tiny">
@@ -15,11 +15,11 @@ export const renderItem = item => {
             </button>
         </li>
     `;
-    elements.shopping.insertAdjacentHTML('beforehand', markup);
+    elements.shopping.insertAdjacentHTML('beforeend', markup);
 };
 
 export const deleteItem = id => {
     const item = document.querySelector(`[data-itemid="${id}"]`);    
-    item.parentElement.removeChild(item);
+    if (item) item.parentElement.removeChild(item);
 };
 
