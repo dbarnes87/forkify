5cf647a4cedef74f14d2aa983f9ce469b8406487
David Barnes <david.barnes115@gmail.com>
2019-10-03 14:47:56 -0400

    finished/run npm build

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,6 @@ import { elements, renderLoader, clearLoader } from './views/base';
  * -Liked recipes
  * */
 const state = {};
-window.state = state;
 
 /*
 * SEARCH CONTROLLER
@@ -71,7 +70,6 @@ elements.searchResPages.addEventListener('click', e => {
 const controlRecipe = async () => {
     // Get ID from url
     const id = window.location.hash.replace('#', '');
-    console.log(id);
 
     if (id) {
         // Prepare the UI for changes
@@ -146,7 +144,6 @@ elements.shopping.addEventListener('click', e => {
 /*
 LIKE Controller
 */
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -215,8 +212,6 @@ elements.recipe.addEventListener('click', e => {
         controlLike();
     }
 });
-
-window.l = new List();
 
 
 
