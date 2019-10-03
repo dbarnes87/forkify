4c7fdd4b05394b281c4c9494e2b184a8ce81d2f7
David Barnes <david.barnes115@gmail.com>
2019-10-03 10:29:06 -0400

    created list model and ablility to update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 
@@ -116,8 +117,9 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     }
-    console.log(state.recipe);
 });
+
+window.l = new List();
 
 
 
